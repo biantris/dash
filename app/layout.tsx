2e7c6d0966b520e7d24041c21e39aac0c30d5cfa
biantris <anabeatrizxoliveira@gmail.com>
2022-12-07 16:10:46 -0300

    feat(layout): add layout and dashboard component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
-import './globals.css'
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html>
       <head />
       <body>{children}</body>
     </html>
