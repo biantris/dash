94925fe186ad61bf22d918dd7878ded9f082f19b
biantris <anabeatrizxoliveira@gmail.com>
2022-12-08 21:05:06 -0300

    feat(l): improve home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { Card, Col, Row, Typography } from 'antd';
 import CarouselAssets from '../components/carousel';
+import NavLink from './nav-link';
 
-const { Title } = Typography;
+const { Title, Link } = Typography;
 
 export default function Home() {
   return (
     <>
-      <h3>Dashboard route</h3>
+      <h3>Dashboard</h3>
       <Row>
         <Col style={{ padding: 5 }}>
           <Card
@@ -20,6 +21,9 @@ export default function Home() {
           >
             <Title level={4}>Ativos</Title>
             <Title>100</Title>
+            <NavLink href="/assets">
+              <Link style={{}}>ver detalhes</Link>
+            </NavLink>
           </Card>
         </Col>
         <Col style={{ padding: 5 }}>
@@ -32,7 +36,10 @@ export default function Home() {
             }}
           >
             <Title level={4}>Usuários</Title>
-            <Title>100</Title>
+            <Title style={{ paddingLeft: 5 }}>100</Title>
+            <NavLink href="/users">
+              <Link>ver detalhes</Link>
+            </NavLink>
           </Card>
         </Col>
         <Col style={{ padding: 5 }}>
@@ -45,7 +52,10 @@ export default function Home() {
             }}
           >
             <Title level={4}>Unidades</Title>
-            <Title>100</Title>
+            <Title style={{ paddingLeft: 10 }}>100</Title>
+            <NavLink href="/units">
+              <Link>ver detalhes</Link>
+            </NavLink>
           </Card>
         </Col>
         <Col style={{ padding: 5 }}>
@@ -59,6 +69,9 @@ export default function Home() {
           >
             <Title level={4}>Empresas</Title>
             <Title>100</Title>
+            <NavLink href="/companies">
+              <Link>ver detalhes</Link>
+            </NavLink>
           </Card>
         </Col>
 
@@ -66,6 +79,9 @@ export default function Home() {
           <Card style={{ width: 500, height: 500 }}>
             <Title level={4}>Ativos | Status</Title>
             <CarouselAssets />
+            <NavLink href="/assets">
+              <Link>ver detalhes</Link>
+            </NavLink>
           </Card>
         </Col>
 
@@ -73,6 +89,9 @@ export default function Home() {
           <Card style={{ width: 650, height: 500 }}>
             <Title level={4}>Ativos | Métricas</Title>
             {/* <GraphBarChart/> */}
+            <NavLink href="/assets">
+              <Link>ver detalhes</Link>
+            </NavLink>
           </Card>
         </Col>
       </Row>
