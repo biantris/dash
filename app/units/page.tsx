1003a7fa2efdad64ea32b8f1ebf5b9e691423651
biantris <anabeatrizxoliveira@gmail.com>
2022-12-09 03:00:51 -0300

    feat(list): add fetch data in pages

--- app/units/page.tsx
+++ app/units/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <h3>Units route</h3>;
+export default async function Page() {
+  const res = await fetch(
+    `https://my-json-server.typicode.com/tractian/fake-api/units`
+  );
+  const data: any = await res.json();
+
+  return (
+    <>
+      <h3>Unidades</h3>
+      {data &&
+        data.map((item: any) => {
+          return (
+            <>
+              <p>
+                <b>Nome:</b> {item.name}
+              </p>
+              <p>
+                <b>id:</b> {item.id}
+              </p>
+              <p>
+                <b>companyId:</b> {item.companyId}
+              </p>
+              <hr />
+            </>
+          );
+        })}
+    </>
+  );
 }
